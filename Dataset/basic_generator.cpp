dd67375f8e87801579c7875397962d9e9cbe2969
zezoo050 <ziad20037891@gmail.com>
2025-04-16 12:54:14 +0200

    Added consecutive to disallow both "text" and "title" to be two terms consecutive

--- Dataset/basic_generator.cpp
+++ Dataset/basic_generator.cpp
@@ -2,7 +2,6 @@
 // Created by ziad on 3/1/25.
 //
 
-
 #include <filesystem>
 #include <fstream>
 #include <iostream>
@@ -14,82 +13,106 @@
 using namespace std;
 
 void load_rules(const string &rules_location, unordered_map<string, bool> &is_term,
-                unordered_map<string, vector<string> > &children) {
+                unordered_map<string, vector<string>> &children, unordered_map<string, bool> &consecutive)
+{
     ifstream file(rules_location);
-    if (!file.is_open()) {
+    if (!file.is_open())
+    {
         throw runtime_error("Could not open DSL rules file: " + rules_location);
     }
 
     nlohmann::json rules;
     file >> rules;
 
-    for (auto &[key, value]: rules.items()) {
+    for (auto &[key, value] : rules.items())
+    {
         is_term[key] = value["term"];
-        for (const auto &child: value["children"]) {
+        consecutive[key] = value["consecutive"];
+        for (const auto &child : value["children"])
+        {
             children[key].push_back(child);
         }
     }
 }
 
 void generate(const string &cur_node, string &dsl, mt19937 &rng, unordered_map<string, bool> &is_term,
-              unordered_map<string, vector<string> > &children) {
-    if (is_term[cur_node]) {
+              unordered_map<string, vector<string>> &children, unordered_map<string, bool> &consecutive, string &last)
+{
+    if (is_term[cur_node])
+    {
         dsl += cur_node;
+        last = cur_node;
     }
-    if (children[cur_node].empty()) {
+    if (children[cur_node].empty())
+    {
         return;
     }
-    if (is_term[cur_node]) {
+    if (is_term[cur_node])
+    {
         dsl += "{";
     }
 
     int num_children = 1;
-    if (cur_node=="root" or is_term[cur_node]) {
+    if (cur_node == "root" or is_term[cur_node])
+    {
         num_children = uniform_int_distribution(1, 4)(rng);
     }
-    for (int i = 0; i < num_children; i++) {
-        const string to = children[cur_node][uniform_int_distribution(0, (int) children[cur_node].size() - 1)(rng)];
-        generate(to, dsl, rng, is_term, children);
-        if (i + 1 != num_children) {
+    for (int i = 0; i < num_children; i++)
+    {
+        const string to = children[cur_node][uniform_int_distribution(0, (int)children[cur_node].size() - 1)(rng)];
+        if (to == last and !consecutive[to])
+        {
+            i--;
+            continue;
+        }
+        generate(to, dsl, rng, is_term, children, consecutive, last);
+        if (i + 1 != num_children)
+        {
             dsl += ",";
         }
     }
-    if (is_term[cur_node]) {
+    if (is_term[cur_node])
+    {
         dsl += "}";
     }
 }
 
-
-signed main(int argc, char *argv[]) {
-    if (argc < 6) {
+signed main(int argc, char *argv[])
+{
+    if (argc < 6)
+    {
         cerr << "Need 5 arguments: number of samples, folder location, file counter, rules location, and seed\n";
         return 1;
     }
 
-    try {
-        unordered_map<string, vector<string> > children;
+    try
+    {
+        unordered_map<string, vector<string>> children;
         unordered_map<string, bool> is_term;
+        unordered_map<string, bool> consecutive;
         unsigned int num_samples = atoi(argv[1]);
         string folder_location = argv[2];
         unsigned int file_counter = atoi(argv[3]);
         string rules_location = argv[4];
         unsigned int seed = atoi(argv[5]);
 
-        load_rules(rules_location, is_term, children);
+        load_rules(rules_location, is_term, children, consecutive);
 
         mt19937 rng(seed);
 
-        for (int i = 0; i < num_samples; i++) {
+        for (int i = 0; i < num_samples; i++)
+        {
             ofstream file(folder_location + "/" + to_string(file_counter++) + ".gui");
-            string content;
-            generate("root", content, rng, is_term, children);
+            string content, last;
+            generate("root", content, rng, is_term, children, consecutive, last);
             file << content;
         }
-    } catch (const std::exception &e) {
+    }
+    catch (const std::exception &e)
+    {
         std::cerr << "Error " << e.what() << "\n";
         return 1;
     }
-
 
     return 0;
 }
