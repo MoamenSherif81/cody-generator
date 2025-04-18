f500e854ae22ab1b3901b50ec3ca5aaf2d6675f8
Mohab Ashraf <a.mohab2000@gmail.com>
2025-04-19 01:23:30 +0200

    updata requirments file

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from app.routers import user, project, record, dsl
+
 from app.config.database import engine, Base
+from app.routers import user, project, record
 
 app = FastAPI(title="Cody-generator BackEnd")
 
@@ -15,7 +16,7 @@ Base.metadata.create_all(bind=engine)
 app.include_router(user.router)
 app.include_router(project.router)
 app.include_router(record.router)
-app.include_router(dsl.router)
+# app.include_router(dsl.router)
 
 @app.get("/", summary="Root endpoint", description="Welcome message for the Code Generator API")
 def read_root():
--- app/services/ai_service.py
+++ app/services/ai_service.py
@@ -1,12 +1,10 @@
-from PIL import Image
 from fastapi import UploadFile
 from Compiler_V2 import lint_dsl, compile_dsl
-import io
 
 
 def process_screenshot(uploaded_file: UploadFile):
-    image_bytes = uploaded_file.file.read()
-    image = Image.open(io.BytesIO(image_bytes))
+    # image_bytes = uploaded_file.file.read()
+    # image = Image.open(io.BytesIO(image_bytes))
 
     #todo: Placeholder: Call AI API to convert screenshot to DSL
     dsl = "row{box{text,input}},row{box{input,select-box},box{title},box{button}},row{box{text,input}}"
