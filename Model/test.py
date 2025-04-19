87f4bae37af65806abf759be79d2b6fbb2136662
zezoo050 <ziad20037891@gmail.com>
2025-04-19 17:10:32 +0200

    Added more testing functionalities

--- Model/test.py
+++ Model/test.py
@@ -10,12 +10,83 @@ from classes.Sampler import Sampler
 from Utils import Utils
 import tensorflow as tf
 
+
 def token_level_accuracy(original, predicted):
     length_sum = 0
     match_sum = 0
     for idx, original_tokens in enumerate(original):
         predicted_tokens = predicted[idx]
-        
+        length_sum += max(len(original_tokens), len(predicted_tokens))
+        for i in range(min(len(original_tokens), len(predicted_tokens))):
+            if original_tokens[i] == predicted_tokens[i]:
+                match_sum += 1
+    print("Token Level Accuracy {}".format(match_sum / length_sum))
+
+
+def LCS_accuracy(original, predicted):
+    length_sum = 0
+    match_sum = 0
+    for idx, original_tokens in enumerate(original):
+        predicted_tokens = predicted[idx]
+        length_sum += len(original_tokens)
+        n = len(original_tokens)
+        m = len(predicted_tokens)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for idx_i in range(n):
+            for idx_j in range(m):
+                if original_tokens[idx_i] == predicted_tokens[idx_j]:
+                    dp[idx_i][idx_j] = (
+                        dp[idx_i - 1][idx_j - 1] + 1
+                        if idx_i - 1 >= 0 and idx_j - 1 >= 0
+                        else 1
+                    )
+                else:
+                    dp[idx_i][idx_j] = max(
+                        dp[idx_i - 1][idx_j] if idx_i - 1 >= 0 else 0,
+                        dp[idx_i][idx_j - 1] if idx_j - 1 >= 0 else 0,
+                    )
+        match_sum += dp[n - 1][m - 1]
+    print("LCS Accuracy {}".format(match_sum / length_sum))
+
+
+def WER_accuracy(original, predicted):
+    differance_sum = 0
+    percentage = 0
+    for idx, original_tokens in enumerate(original):
+        predicted_tokens = predicted[idx]
+        n = len(original_tokens)
+        m = len(predicted_tokens)
+        # max number, too lazy to add a variable, 5000 should be enough
+        dp = [[5000] * (n + 1) for _ in range(m + 1)]
+        dp[0][0] = 0
+        for idx_i in range(n):
+            for idx_j in range(m):
+                if original_tokens[idx_i] == predicted_tokens[idx_j]:
+                    dp[idx_i + 1][idx_j + 1] = min(
+                        dp[idx_i + 1][idx_j + 1], dp[idx_i][idx_j]
+                    )
+                else:
+                    dp[idx_i + 1][idx_j] = min(
+                        dp[idx_i + 1][idx_j], dp[idx_i][idx_j] + 1
+                    )
+                    dp[idx_i][idx_j + 1] = min(
+                        dp[idx_i][idx_j + 1], dp[idx_i][idx_j] + 1
+                    )
+                    dp[idx_i + 1][idx_j + 1] = min(
+                        dp[idx_i + 1][idx_j + 1], dp[idx_i][idx_j] + 1
+                    )
+        differance_sum += dp[n][m]
+        percentage += dp[n][m] / len(predicted_tokens)
+    print(
+        "WER(Word Error Rate) Accuracy (on average, how many tokens needs to be changed for predicred to be original) {}".format(
+            differance_sum / len(original)
+        )
+    )
+    print(
+        "WER(Word Error Rate) Accuracy (on average, how much percentage of the predicred needs to be changed to be original) {}".format(
+            percentage / len(original)
+        )
+    )
 
 
 def test_gready(testing_path, model, sampler):
@@ -34,6 +105,36 @@ def test_gready(testing_path, model, sampler):
                 predicted.append(sampler.predict_greedy(model, np.array([img])))
     assert len(original) == len(predicted)
     print("greedy testing")
+    token_level_accuracy(original, predicted)
+    LCS_accuracy(original, predicted)
+    WER_accuracy(original, predicted)
+    print("------------------------------------------")
+
+
+def test_beam_search(testing_path, beam_width, model, sampler):
+    original = []
+    predicted = []
+    for f in os.listdir(testing_path):
+        if f.find(".gui") != -1:
+            gui = open("{}/{}".format(testing_path, f), "r")
+            file_name = f[: f.find(".gui")]
+
+            if os.path.isfile("{}/{}.png".format(testing_path, file_name)):
+                img = Utils.get_preprocessed_img(
+                    "{}/{}.png".format(testing_path, file_name), IMAGE_SIZE
+                )
+                original.append(gui)
+                predicted.append(
+                    sampler.predict_beam_search(
+                        model, np.array([img], beam_width=beam_width)
+                    )
+                )
+    assert len(original) == len(predicted)
+    print("Beam search with k={} testing".format(beam_width))
+    token_level_accuracy(original, predicted)
+    LCS_accuracy(original, predicted)
+    WER_accuracy(original, predicted)
+    print("------------------------------------------")
 
 
 def run(
@@ -92,6 +193,9 @@ def run(
         model.evaluate_generator(testing_dataset, steps=testing_steps_per_epoch)
 
     sampler = Sampler(weights_path, input_shape, output_size, CONTEXT_LENGTH)
+    test_gready(testing_path, model, sampler)
+    test_beam_search(testing_path, 3, model, sampler)
+    test_beam_search(testing_path, 5, model, sampler)
 
 
 if __name__ == "__main__":
