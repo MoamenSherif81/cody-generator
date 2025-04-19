9bbcd7bcb95bbb9375047d1f0e58f78bd819cadd
zezoo050 <ziad20037891@gmail.com>
2025-04-19 11:18:15 +0200

    fixing some imports

--- Model/classes/dataset/Generator.py
+++ Model/classes/dataset/Generator.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from classes.Vocabulary import START_TOKEN, END_TOKEN, PLACEHOLDER
 from .Dataset import Dataset
-from models.config import IMAGE_SIZE, CONTEXT_LENGTH
+from classes.models.config import IMAGE_SIZE, CONTEXT_LENGTH
 from Utils import Utils
 
 
