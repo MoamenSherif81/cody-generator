a619417774041fd2ec7116b4bb1cc8682f514d28
Mohab Ashraf <a.mohab2000@gmail.com>
2025-05-25 00:08:05 +0300

    refactor google_sheet service in app module

--- app/services/google_sheet.py
+++ app/services/google_sheet.py
@@ -4,19 +4,23 @@ from datetime import datetime
 import gspread
 
 
-def append_log(userName: str, log: str):
-    credPath = os.path.join(os.getcwd(), "app", "llm-gp-data.json")
+def _init():
+    sheetId = os.getenv("SHEET_ID", "")
+    secretFileName = os.getenv("GOOGLE_SECRET_KEY_FILE", "")
+    credPath = os.path.join(os.getcwd(), secretFileName)
     gc = gspread.service_account(filename=credPath)
-    sh = gc.open_by_key("1Y17nNQ5WUPU5c3QnwPyaGaQvo2jgCVCXGANhrJ_OnQ8")
+    sh = gc.open_by_key(sheetId)
+    return sh
+
+def append_log(userName: str, log: str):
+    sh = _init()
     worksheet = sh.worksheet("Logs")
     timestamp = datetime.now().strftime("%A, %Y-%m-%d %H:%M:%S")
     worksheet.append_row([timestamp, userName, log])
 
 
 def append_record(username, aiModel, situation, dsl, lang):
-    credPath = os.path.join(os.getcwd(), "app", "llm-gp-data.json")
-    gc = gspread.service_account(filename=credPath)
-    sh = gc.open_by_key("1Y17nNQ5WUPU5c3QnwPyaGaQvo2jgCVCXGANhrJ_OnQ8")
+    sh = _init()
     worksheet = sh.worksheet(lang)
     timestamp = datetime.now().strftime("%A, %Y-%m-%d %H:%M:%S")
     worksheet.append_row([username, aiModel, timestamp, situation, dsl])
