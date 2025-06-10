dc664c9a7fc15221385520d46c8da57fb0318031
Mohab Ashraf <a.mohab2000@gmail.com>
2025-06-10 16:18:44 +0300

    remove chat script

--- LLM/Data_v2/genScript_chatgpt.py
+++
@@ -1,7 +0,0 @@
-from openai import OpenAI
-
-client = OpenAI(api_key='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')
-import json
-from LLM.Data_v2.GenerateQuestionsOneLangQuery_chatgpt import GenerateMessage
-
-print(GenerateMessage("/home/mohab/Mohab/GP/cody-generator/LLM/Queries/DSL-Rules.json","english"))
